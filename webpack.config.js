5acb8af7b2828cf3c14e2e1ef19ba39ff60ff9df
kinginthenorthcodez <kinginthenorthcodez@gmail.com>
2022-05-03 00:18:49 +0530

    Run build docs for github pages

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     clean: true,
   },
   devtool: 'inline-source-map',

ae80840cbde0b4d2dcc66350fff9fe4860d9bdb3
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-05-06 13:05:20 -0300

    fix linters errors

--- tests/__tests__/clearUpdateStatuses.test.js
+++ tests/__tests__/clearUpdateStatuses.test.js
@@ -6,61 +6,57 @@ describe('Test update, edit, clearCompleted tasks', () => {
   document.body.innerHTML = `
  <input type="text" id="toDoInput" class="toDo" placeholder="Add to your list..." >
  <ul class="toDoItems"></ul>`;
-  const toDoItems = document.querySelector('.toDoItems');
   const toDoInput = document.querySelector('#toDoInput');
   toDoInput.value = 'item1';
   const arr = tasks.getItem('Tasks') || [];
-    const newTask = {
-      description: 'Task1',
-      completed: false,
-      index: arr.length + 1,
-    };
-    arr.push(newTask);
-    const tasky = {
-      description: 'Task2',
-      completed: false,
-      index: arr.length + 1,
-    };
+  const newTask = {
+    description: 'Task1',
+    completed: false,
+    index: arr.length + 1,
+  };
+  arr.push(newTask);
+  const tasky = {
+    description: 'Task2',
+    completed: false,
+    index: arr.length + 1,
+  };
 
-    arr.push(tasky);
-    tasks.setItem('Tasks', arr);
+  arr.push(tasky);
+  tasks.setItem('Tasks', arr);
 
-    test('Edit a Task', () => {
-      const editTask = (task, textUpdate) => {
-        task.description = textUpdate;
-        tasks.setItem('Tasks', arr);
-      };
-      const textUpdate = 'This is the updates';
-      expect(newTask.description).not.toEqual('This is the updates');
-      editTask(newTask, textUpdate);
-      expect(newTask.description).toEqual('This is the updates');
-    });
-
-
-   test('Updated completed task status', () => {
-     const updateState = (index) => {
-       if(arr[index].completed === false){
-        arr[index].completed = true;
-       } else {
-         arr[index].completed = false;
-       }
+  test('Edit a Task', () => {
+    const editTask = (task, textUpdate) => {
+      task.description = textUpdate;
       tasks.setItem('Tasks', arr);
-    }
+    };
+    const textUpdate = 'This is the updates';
+    expect(newTask.description).not.toEqual('This is the updates');
+    editTask(newTask, textUpdate);
+    expect(newTask.description).toEqual('This is the updates');
+  });
 
-    let store = tasks.getItem('Tasks');
-    updateState(1)
+  test('Updated completed task status', () => {
+    const updateState = (index) => {
+      if (arr[index].completed === false) {
+        arr[index].completed = true;
+      } else {
+        arr[index].completed = false;
+      }
+      tasks.setItem('Tasks', arr);
+    };
+
+    updateState(1);
     expect(arr[1].completed).toEqual(true);
-   })
+  });
 
-    test('clear completed tasks', () => {
-      const clearCompleted = () => {
-        const todoosArr = tasks.getItem('Tasks');
-        const data = todoosArr.filter((item) => item.completed !== true);
-        tasks.setItem('Tasks', data);
-      };
+  test('clear completed tasks', () => {
+    const clearCompleted = () => {
+      const todoosArr = tasks.getItem('Tasks');
+      const data = todoosArr.filter((item) => item.completed !== true);
+      tasks.setItem('Tasks', data);
+    };
 
-      clearCompleted();
-      expect(tasks.getItem('Tasks')).toEqual([ { description: 'This is the updates', completed: false, index: 1 } ]);
-    });
-
+    clearCompleted();
+    expect(tasks.getItem('Tasks')).toEqual([{ description: 'This is the updates', completed: false, index: 1 }]);
+  });
 });
